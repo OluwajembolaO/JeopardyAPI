5f2502bd41b15f058a614db92190bce2b62eb31e
Jem <116439666+OluwajembolaO@users.noreply.github.com>
2024-08-18 14:49:09 -0400

    EdgeCasesLogic

--- caseLogic.py
+++ caseLogic.py
@@ -1,17 +1,28 @@
 # File to handle all the edge cases in application
 
 # Checks to see if it's the correct type 
-
 def checkType(type):
-    pass
+    # Convert the input to lowercase and check if it is 'concept' or 'memorization'
+    if type.lower() not in ['concept', 'memorization']:
+        return False
+    else:
+        return True
 
 # Checks to see if the levels are correct
 def checkLevels(levels):
-    pass
+    # Check if the levels are within the valid range.
+    # The range is from 200 to 800 and should be a multiple of 100.
+    if levels < 200 or levels > 800 or levels % 100 != 0:
+        return False
+    
+    return True
+
 
 # Checks to see if the subcatogeries are correct
 def checkSubcatogeries(subcatogeries):
-    pass
+    wordCount = subcatogeries.count(',') + 1   
+    # check if wordCount is in the range of [1,7] 
+    return 0 < wordCount < 8
 
 # Checks to see if the topic is correct
 def checkTopic(topic):
@@ -23,15 +34,20 @@ def checkContext(context):
 
 # Check to see if the difficulity is correct
 def checkDifficulity(difficulity):
-    pass
+    # The valid difficulty levels are:
+    # elementary school, middle school, high school, ap, college, phd
+    valid_levels = ['elementary school', 'middle school', 'high school', 'ap', 'college', 'phd']
+
+    # Convert the input to lowercase and check if it is in the valid levels
+    if difficulity.lower() not in valid_levels:
+        return False
+    else:
+        return True
 
 # check to see if's the correct subcategory count
 def checkSubcategoryCount(subcategoryCount):
-    pass
+    return subcategoryCount < 8
 
 # check to see if the subcategory count is greater or equal to the subcatogeries given
 def checkSubcategoryCountGreater(subcategoryCount, subcatogeries):
-    if subcategoryCount >= subcatogeries:
-        raise ValueError("The number of subcategories must be less than or equal to the number of subcategories given")
-    else:
-        pass
+    return subcategoryCount >= subcatogeries
